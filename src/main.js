0d29e3ad26636bdfe9ee58ecb7cf37412fc40770
Carlos Córdova <ccordova.zosma@gmail.com>
2021-04-14 11:19:15 -0500

    config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Vuelidate from "vuelidate";
+import App from './App.vue';
+import axios from "axios";
+import router from './router';
+import store from './store';
+import VueAxios from "vue-axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import Buefy from 'buefy';
 
+import "@/assets/styles/main.scss";
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+library.add(fab);
+library.add(fas);
 Vue.config.productionTip = false
+
+Vue.use(Buefy, {
+  defaultIconComponent: "font-awesome-icon",
+  defaultIconPack: "fas"
+});
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://weeare-api.beneficios.pe/regalos/api"
+    : "http://weeare-api.beneficios.pe/regalos/api";
+Vue.use(VueAxios, axios);
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
